53691f3910c4feb89a199a6098baebc289e61e51
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-08-25 17:45:19 -0300

    Atualizacao github

---
+++ TPA-Lista-01-Exercicio-01/TPA-Lista-01-Exercicio-01/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// 1)	Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exibindo números entre 1 e 20, um abaixo do outro");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8");
+            Console.WriteLine("9");
+            Console.WriteLine("10");
+            Console.WriteLine("11");
+            Console.WriteLine("12");
+            Console.WriteLine("13");
+            Console.WriteLine("14");
+            Console.WriteLine("15");
+            Console.WriteLine("16");
+            Console.WriteLine("17");
+            Console.WriteLine("18");
+            Console.WriteLine("19");
+            Console.WriteLine("20");
+        }
+
+    }
+}
+
